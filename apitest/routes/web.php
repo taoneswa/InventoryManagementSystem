a6f7da995f96057f170d9ba0ce72174e942c8f2f
taoneswa kasirai <61433155+taoneswa@users.noreply.github.com>
2024-04-13 09:42:40 +0200

    api protect

--- apitest/routes/web.php
+++ apitest/routes/web.php
@@ -14,25 +14,34 @@ use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\TransactionsController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/users', UserController::class);
+    Route::apiResource('api/employee', EmployeeController::class);
+    Route::resource('/attendance', AttendanceController::class);
+    Route::resource('/brand',BrandController::class);
+    Route::resource('/category', CategoryController::class);
+    Route::resource('/customer', CustomersController::class);
+    Route::resource('/ordering', OrderingController::class);
+    Route::resource('/product',ProductController::class);
+    Route::apiResource('api/reports',ReportsController::class);
+    Route::resource('/salary',SalaryController::class);
+    Route::resource('/settings',SettingsController::class);
+    Route::resource('/stock',StockController::class);
+    Route::resource('/supplier', SupplierController::class);
+    Route::resource('/transactions', TransactionsController::class);
 });
-Route::apiResource('api/employee', EmployeeController::class);
-Route::resource('/attendance', AttendanceController::class);
-Route::resource('/brand',BrandController::class);
-Route::resource('/category', CategoryController::class);
-Route::resource('/customer', CustomersController::class);
-Route::resource('/ordering', OrderingController::class);
-Route::resource('/product',ProductController::class);
-Route::apiResource('api/reports',ReportsController::class);
-Route::resource('/salary',SalaryController::class);
-Route::resource('/settings',SettingsController::class);
-Route::resource('/stock',StockController::class);
-Route::resource('/supplier', SupplierController::class);
-Route::resource('/transactions', TransactionsController::class);
+
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
